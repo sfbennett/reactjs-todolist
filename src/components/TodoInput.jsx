1a662b9ea7c9bdeb855c79d3fd74eb1097f84b8c
sbennett <susannah-bennett@hotmail.com>
2024-06-03 22:07:36 +0100

    Add todo list files

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useState } from "react";
+
+export default function TodoInput(props) {
+  const { handleAddTodos, todoValue, setTodoValue } = props;
+
+  return (
+    <header>
+      <input
+        className="input"
+        value={todoValue}
+        onChange={(e) => {
+          setTodoValue(e.target.value);
+        }}
+        placeholder="Enter to do..."
+      />
+      <button
+        onClick={() => {
+          handleAddTodos(todoValue);
+          setTodoValue("");
+        }}
+      >
+        Add
+      </button>
+    </header>
+  );
+}
